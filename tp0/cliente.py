831e0f0607b5b6c36144f6492eae92d482c0a0b0
Hugo Sousa <hsousa@gmx.com>
2018-03-14 20:42:38 -0300

    tp0: servidor funcional para 1 cliente

--- tp0/cliente.py
+++ tp0/cliente.py
@@ -1,14 +1,68 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 '''
-Trabalho Prático 0: Redes de Computadores DCC023
+Trabalho Pratico 0: Redes de Computadores DCC023
 Autor: Hugo Araujo de Sousa (2013007463)
-cliente.py: Envia uma string para o servidor e a recebe criptografada.
+cliente.py: Sends a string to the server and gets it back encrypted.
 '''
 
 import argparse as ap
+import socket
+import struct
 
-# Adiciona argumentos de linha de comando.
+# Add command line arguments.
 parser = ap.ArgumentParser()
 
-parser.add_argument('')
+parser.add_argument('ip_server', type=str, help='Server IP address')
+parser.add_argument('port_server', type=int, help='Server port')
+parser.add_argument('input_string', type=str, help='Input string to encrypt')
+parser.add_argument('shift', type=int, \
+	help='Unsigned integer to use in Caesar Cipher')
+
+args = parser.parse_args()
+
+
+def caesar_cipher(input_string, shift):
+	'''
+		Applies the Caesar Cipher to a string.
+
+		@input_string:	String to encode.
+		@shift: Unsigned integer that represents the number of shifts to apply
+			to each character.
+
+		@return: String that represents the input string after Caesar Cipher 
+			encoding.
+	'''
+
+	encoded = ''
+	for i in input_string: encoded = encoded + chr(ord(i) + shift)
+	return encoded
+
+
+def main():
+	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	dest = (args.ip_server, args.port_server)
+	tcp.connect(dest)
+
+	input_string_size = len(args.input_string)
+	
+	# Send size of input string to server.
+	msg = struct.pack('!i', input_string_size)
+	tcp.send(msg)
+
+	# Send the encoded string to server.
+	msg = caesar_cipher(args.input_string, args.shift)
+	tcp.send(msg)
+
+	# Send the Caesar Cipher shift to server.
+	msg = struct.pack('!i', args.shift)
+	tcp.send(msg)
+
+	# Receive string from server.
+	server_string = tcp.recv(input_string_size)
+
+	print 'Received \'' + server_string + '\' from server.'
+	tcp.close()
+
+
+main()
--- tp0/servidor.py
+++ tp0/servidor.py
@@ -1,8 +1,63 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 '''
-Trabalho Prático 0: Redes de Computadores DCC023
+Trabalho Pratico 0: Redes de Computadores DCC023
 Autor: Hugo Araujo de Sousa (2013007463)
-servidor.py: Recebe uma string de cada cliente, as criptografa e envia a versão
-criptografada para o cliente.
+servidor.py: Receives a string from each client, decodes it and sends it back
+to client.
 '''
+
+import argparse as ap
+import socket
+import struct
+
+# Add command line arguments.
+parser = ap.ArgumentParser()
+parser.add_argument('port_server', type=int, help='Server port')
+args = parser.parse_args()
+
+
+def decode_caesar(input_string, shift):
+	'''
+		Decodes a string encrypted with the Caesar Cipher.
+
+		@input_string:	String to decode.
+		@shift: Unsigned integer that represents the number of shifts applied to
+			each character.
+
+		@return: String that represents the input string after Caesar Cipher 
+			decoding.
+	'''
+
+	decoded = ''
+	for i in input_string: decoded = decoded + chr(ord(i) - shift)
+	return decoded
+
+
+def main():
+	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	orig = ('', args.port_server)
+	tcp.bind(orig)
+	tcp.listen(1)
+	con, client = tcp.accept()
+
+	# Receive the string size from the client.
+	msg = con.recv(4)
+	string_size = struct.unpack('!i', msg)[0]
+
+	# Receive the encoded string from the client.
+	encoded_string = con.recv(string_size)
+
+	# Receive the Caesar Cipher shift value from the client.
+	msg = con.recv(4)
+	caesar_shift = struct.unpack('!i', msg)[0]
+
+	decoded = decode_caesar(encoded_string, caesar_shift)
+	print 'Decoded string:', decoded
+
+	# Send decoded string back to client.
+	con.send(decoded)
+	tcp.close()
+
+
+main()
