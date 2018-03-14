2574b4619a055ad24c9bbef10cb4aefb051aee27
Hugo Sousa <hsousa@gmx.com>
2018-03-14 15:18:15 -0300

    tp0: adicionada estrutura inicial

---
+++ tp0/cliente.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Trabalho Prático 0: Redes de Computadores DCC023
+Autor: Hugo Araujo de Sousa (2013007463)
+cliente.py: Envia uma string para o servidor e a recebe criptografada.
+'''
+
+import argparse as ap
+
+# Adiciona argumentos de linha de comando.
+parser = ap.ArgumentParser()
+
+parser.add_argument('')
---
+++ tp0/servidor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+'''
+Trabalho Prático 0: Redes de Computadores DCC023
+Autor: Hugo Araujo de Sousa (2013007463)
+servidor.py: Recebe uma string de cada cliente, as criptografa e envia a versão
+criptografada para o cliente.
+'''
